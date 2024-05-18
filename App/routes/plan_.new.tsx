ab41f934b65cb44e931986af1af07b6839a710d8
Gerardo <gerardo.fernandezmena@ucr.ac.cr>
2024-05-18 13:35:36 -0600

    Basic layout for new "Plan" section

--- App/routes/plan_.new.tsx
+++ App/routes/plan_.new.tsx
@@ -1,17 +1,88 @@
-import { Form } from "@remix-run/react";
+import { ActionFunctionArgs, LinksFunction, redirect } from "@remix-run/node";
+import { Form, json, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+import appStyles from '../stylesheets/plan_.new.css';
+import icons from "bootstrap-icons/font/bootstrap-icons.css";
+
 
 export default function PlanNew() {
+  const DEFAULT_TOOLTIP_PLAN = "Nombre del plan de estudios"
+  const data = useLoaderData<typeof loader>();
+  const [listaCursos, setListaCursos] = useState(data);
+  const [nombrePlan, setNombrePlan] = useState(DEFAULT_TOOLTIP_PLAN);
+  //const [codigoPlan, setCondigoPlan] = useState("");
+
+  let cursos = Object.values(listaCursos.cursos).map(curso => {
+    return <li key={curso.slice(0, 6)}>{curso}</li>
+  })
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.currentTarget.value !== ""
+      ? setNombrePlan(event.currentTarget.value)
+      : setNombrePlan(DEFAULT_TOOLTIP_PLAN)
+  }
+
+
   return (
-    <div>
-      <Form>
-        <p>
-          <input type="text" name="nombre" placeholder="Nombre del plan de estudios" />
-        </p>
-        <label>
-          Código
-          <input type="text" name="codigo" placeholder="Código" />
-        </label>
+    <div className="container">
+      <Form method="post" autoComplete="off">
+        <span className="d-block">
+          <input
+            title={nombrePlan}
+            type="text"
+            name="nombre"
+            placeholder="Nombre"
+            className="inputTitle mainTitle "
+            onChange={handleChange} />
+            <i className="bi-pencil"></i>
+        </span>
+        <div className="whiteContainer">
+          <div>
+            <label>
+              Código:
+              <input type="text" name="codigo" placeholder="Código" />
+            </label>
+          </div>
+          <div className="listaCursos inputElement">
+            <label>Cursos asociados:</label>
+            <ul className="whiteContainer">
+              {cursos}
+            </ul>
+            <span className="horarios-plan-new-listacursos-buttons">
+              <button type="button">Agregar</button>
+              <button type="button">Eliminar</button>
+              <button type="button">Ver</button>
+            </span>
+          </div>
+
+        </div>
+        <button type="submit">Crear</button>
       </Form>
     </div>
   )
 }
+
+export async function action({ request }: ActionFunctionArgs) {
+  //Validar datos codigo, nombre
+  const formData = await request.formData();
+  const name = String(formData.get("name"));
+  return redirect("/plan")
+}
+
+export function loader() {
+  //Cargar cursos asociados al plan desde el backend
+  const cursos = {
+    0: "IF5584 Introducción a Sistema de Información",
+    1: "IF5583 Introducción a Sistema de Información",
+    2: "IF5585 Introducción a Sistema de Información",
+    3: "IF5587 Introducción a Sistema de Información",
+    4: "IF2342 Fundamentos de Bases de Datos"
+  }
+
+  return json({ cursos: cursos })
+}
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStyles },
+  { rel: "stylesheet", href: icons },
+];
