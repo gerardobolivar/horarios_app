14744f50e0b493efbc1f7b7f7a588b32eb18ae51
Gerardo Vargas <gerardo.fernandezmena@ucr.ac.cr>
2024-08-24 22:07:06 -0600

    H10: Remove empty classrooms

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -3,6 +3,7 @@ import MainTitle from "../shared/MainTitle";
 import CicleCard from "./CicleCard";
 import { bindCicloByHorario, getCiclos } from "prisma/models/cicloModel";
 import { useLoaderData } from "@remix-run/react";
+import { activateHorarioById } from "prisma/models/horarioModel";
 
 export default function HomeAdmin(){
   const data = useLoaderData<typeof loader>();
@@ -23,6 +24,6 @@ return (
 
 export const loader = async ({ params }:LoaderFunctionArgs) => {
   const ciclos = await getCiclos().then((r)=>{return r}).catch((error)=>{return []})
-  //await bindCicloByHorario(2,2);
+  //await bindCicloByHorario(2,2).then(async ()=>{ await activateHorarioById(2);}).catch((e)=>{console.error(e);}) //Dev purposes
   return json({ ciclos: ciclos});
 };
