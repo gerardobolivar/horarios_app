d5b848ae58dd947cfcce1da44f8a2aa92ff8bbc3
Gerardo Vargas <gerardo.fernandezmena@ucr.ac.cr>
2024-11-09 08:24:01 -0600

    Layout changes in Plan module

--- app/routes/plan/route.tsx
+++ app/routes/plan/route.tsx
@@ -3,6 +3,7 @@ import MainTitle from "../shared/MainTitle";
 import PlanCard from "./PlanCard";
 import loaderPlan from "~/.server/Controller/plan/loader";
 import appStyles from '~/stylesheets/plan_.new.css?url';
+import planStyles from './plan.css?url';
 import { LinksFunction } from "@remix-run/node";
 const ROUTE_TAG = "Planes de estudio";
 
@@ -10,15 +11,15 @@ export default function Planes() {
   let data = useLoaderData<typeof loader>();
 
   let planesCards = data.planes.map((plan) => {
-    return <PlanCard key={plan.id_plan_estudio} innerText={plan.nombre_plan} url={`/plan/${plan.id_plan_estudio}`} active={true} />
+    return <PlanCard key={plan.id_plan_estudio} innerText={plan.nombre_plan} url={`/plan/${plan.id_plan_estudio}`} static={false} />
   })
 
   return (
-    <div className="container-sm">
+    <div className="container-sm planContainer">
       <MainTitle innerText={ROUTE_TAG} />
-      <div className="whiteContainer container d-flex">
+      <div className="whiteContainer container">
         {planesCards}
-        <PlanCard innerText="+" url="/plan/new" active={true} />
+        <PlanCard innerText="Nuevo plan" url="/plan/new" static={true} />
       </div>
     </div>
   )
@@ -28,4 +29,5 @@ export const loader = loaderPlan;
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStyles },
+  { rel: "stylesheet", href: planStyles },
 ];
