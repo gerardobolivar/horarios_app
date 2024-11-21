a5b312b20debb5ee4218458089e421af223126e0
Gerardo Vargas <gerardo.fernandezmena@ucr.ac.cr>
2024-11-21 07:30:04 -0600

    Added responsive design for mobile

--- app/routes/plan/PlanCard.jsx
+++ app/routes/plan/PlanCard.jsx
@@ -5,16 +5,16 @@ export default function PlanCard(props) {
   let href = props.url;
   let isStaticAdd = props.static
   return (
-    <div class="card">
-      <div class="card-header">
+    <div className="card">
+      <div className="card-header">
         <h5>{title}</h5>
       </div>
-      <div class="card-body">
+      <div className="card-body">
         {
           !isStaticAdd ? <p className="card-text">Sin información adicional sobre el plan.</p> : 
           <p className="card-text">Crear un nuevo plan de estudios.</p>
         }
-        <a href={href} class="btn mainButton">{isStaticAdd ? "Agregar":"Gestionar"}</a>
+        <a href={href} className="btn mainButton">{isStaticAdd ? "Agregar":"Gestionar"}</a>
       </div>
     </div>
   )
